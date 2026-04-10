a81e68abb3c6885b04488a8262210457b3eec5e4
Mark Lucas <Firstplaysportwv@gmail.com>
2026-04-10 00:06:24 -0400

    fix stripe startup crash

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const nodemailer = require('nodemailer');
 const path = require('path');
 require('dotenv').config();
 
-const stripe = require('stripe')(process.env.STRIPE_SECRET_KEY);
+const stripe = require('stripe')(process.env.STRIPE_SECRET_KEY || 'placeholder');
 
 const app = express();
 
