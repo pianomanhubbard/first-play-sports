e8d2badd797215b8330976cf4db02d64a0d43c52
firstplaysports <firstplaysportswv@gmail.com>
2026-04-13 23:45:49 -0400

    Update server.js

--- server.js
+++ server.js
@@ -19,21 +19,21 @@ const sessions = [
   { id: '2',  sport: 'baseball', icon: '⚾', name: 'Baseball Basics', day: 'Sat Apr 25', time: '11:00 AM – 12:00 PM', taken: 0, total: 6 },
   { id: '3',  sport: 'baseball', icon: '⚾', name: 'Baseball Basics', day: 'Sat Apr 25', time: '12:00 PM – 1:00 PM',  taken: 0, total: 6 },
   { id: '4',  sport: 'baseball', icon: '⚾', name: 'Baseball Basics', day: 'Sat Apr 25', time: '1:00 PM – 2:00 PM',   taken: 0, total: 6 },
-  { id: '5',  sport: 'baseball', icon: '⚾', name: 'Baseball Basics', day: 'Sat Apr 25', time: '2:00 PM – 3:00 PM',   taken: 0, total: 6 },
+  { id: '5',  sport: 'football', icon: '🏈', name: 'Flag Football', day: 'Sat Apr 25', time: '2:00 PM – 3:00 PM',   taken: 0, total: 6 },
   // --- Sun Apr 26 ---
   { id: '6',  sport: 'baseball', icon: '⚾', name: 'Baseball Basics', day: 'Sun Apr 26', time: '10:00 AM – 11:00 AM', taken: 0, total: 6 },
   { id: '7',  sport: 'baseball', icon: '⚾', name: 'Baseball Basics', day: 'Sun Apr 26', time: '11:00 AM – 12:00 PM', taken: 0, total: 6 },
-  { id: '8',  sport: 'baseball', icon: '⚾', name: 'Baseball Basics', day: 'Sun Apr 26', time: '12:00 PM – 1:00 PM',  taken: 0, total: 6 },
+  { id: '8',  sport: 'football', icon: '🏈', name: 'Flag Football', day: 'Sun Apr 26', time: '12:00 PM – 1:00 PM',  taken: 0, total: 6 },
   // --- Sat May 16 ---
   { id: '9',  sport: 'baseball', icon: '⚾', name: 'Baseball Basics', day: 'Sat May 16', time: '10:00 AM – 11:00 AM', taken: 0, total: 6 },
   { id: '10', sport: 'baseball', icon: '⚾', name: 'Baseball Basics', day: 'Sat May 16', time: '11:00 AM – 12:00 PM', taken: 0, total: 6 },
   { id: '11', sport: 'baseball', icon: '⚾', name: 'Baseball Basics', day: 'Sat May 16', time: '12:00 PM – 1:00 PM',  taken: 0, total: 6 },
   { id: '12', sport: 'baseball', icon: '⚾', name: 'Baseball Basics', day: 'Sat May 16', time: '1:00 PM – 2:00 PM',   taken: 0, total: 6 },
-  { id: '13', sport: 'baseball', icon: '⚾', name: 'Baseball Basics', day: 'Sat May 16', time: '2:00 PM – 3:00 PM',   taken: 0, total: 6 },
+  { id: '13', sport: 'football', icon: '🏈', name: 'Flag Football', day: 'Sat May 16', time: '2:00 PM – 3:00 PM',   taken: 0, total: 6 },
   // --- Sun May 17 ---
   { id: '14', sport: 'baseball', icon: '⚾', name: 'Baseball Basics', day: 'Sun May 17', time: '10:00 AM – 11:00 AM', taken: 0, total: 6 },
   { id: '15', sport: 'baseball', icon: '⚾', name: 'Baseball Basics', day: 'Sun May 17', time: '11:00 AM – 12:00 PM', taken: 0, total: 6 },
-  { id: '16', sport: 'baseball', icon: '⚾', name: 'Baseball Basics', day: 'Sun May 17', time: '12:00 PM – 1:00 PM',  taken: 0, total: 6 },
+  { id: '16', sport: 'football', icon: '🏈', name: 'Flag Football', day: 'Sun May 17', time: '12:00 PM – 1:00 PM',  taken: 0, total: 6 },
 ];
 
 
